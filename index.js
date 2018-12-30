168adb56334c6108bf7d2f1752bc82e3e72c7b4d
Raissa Ratkiewicz <raissa_shy@hotmail.com>
2018-12-30 15:56:20 -0300

    atualizando components

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const app = express();
-const router = require('./router');
-const mongoose = require('mongoose');
-mongoose.set('debug', true)
-mongoose.Promise = global.Promise
+const router = require("./router");
+const mongoose = require("mongoose");
+const config = require("./config");
+mongoose.set("debug", true);
 
-mongoose.connect('mongodb://localhost:/auth')
+mongoose.connect(
+  config.mongoURI,
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
 
-app.use(morgan('combined'));
-app.use(bodyParser.json({ type: '*/*' }));
+app.use(morgan("combined"));
+app.use(bodyParser.json({ type: "*/*" }));
 router(app);
-
 
 const PORT = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(PORT);
-console.log('Server Listenning on PORT ' + PORT);
-
-
-
-
+console.log("Server Listenning on PORT " + PORT);
